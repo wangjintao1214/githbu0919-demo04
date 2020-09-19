d1edbccbb1521c8d0eb27b7d21aa11e1a9896782
sunkuo1106 <sunkuo1106@163.com>
2020-09-19 19:50:09 +0800

    sunkuo提交的PetSys2

---
+++ PetSys2/src/main/java/com/kgc/ssm/service/PetService.java
@@ -0,0 +1,11 @@
+package com.kgc.ssm.service;
+
+import com.kgc.ssm.pojo.Pet;
+
+import java.util.List;
+
+public interface PetService {
+    List<Pet>selectAll(Integer type);
+
+    void add(Pet pet);
+}
